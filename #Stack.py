00def2ca40a51cc8331e3f51143754d567d2fe52
yxsh1401 <160396107+yxsh1401@users.noreply.github.com>
2024-12-12 18:10:52 +0400

    Add files via upload

---
+++ #Stack.py
@@ -0,0 +1,16 @@
+#Stack
+s = []
+#pushing elements in
+s.append('a')
+s.append('b')
+s.append('c')
+#Printing the stack
+print(s)
+#Popping elements
+s.pop()
+#checking stack
+print(s)
+#empty stack
+s.pop()
+s.pop()
+print(s)
